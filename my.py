788c0e2d1a6bbf897846f179c5dbcca57b174a13
gulaboarts <tanishshukla956@gmail.com>
2026-03-10 22:59:35 +0530

    Update my.py

--- my.py
+++ my.py
@@ -1,20 +1,9 @@
 import qrcode
 
-# Apne HTML front page ka URL yaha daalo
-url = "https://tanishshukla280810.github.io/jatin-qr-code-work/"  
+url = "https://gulaboarts.github.io/qr-code-/"
 
-# QR code generate karo
-qr = qrcode.QRCode(
-    version=1,
-    error_correction=qrcode.constants.ERROR_CORRECT_H,
-    box_size=10,
-    border=4,
-)
+img = qrcode.make(url)
 
-qr.add_data(url)
-qr.make(fit=True)
+img.save("gulabo_qr.png")
 
-img = qr.make_image(fill_color="black", back_color="white")
-img.save("frontpage_qr.png")
-
-print("QR Code bana liya! frontpage_qr.png file check karo.")
+print("QR Code Ready")
